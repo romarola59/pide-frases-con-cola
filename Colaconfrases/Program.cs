75ff580f8eaba01220edc2b2fe3d82e4bbc76402
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-25 00:28:12 -0300

    Agregar archivos de proyecto.

---
+++ Colaconfrases/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Colaconfrases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
