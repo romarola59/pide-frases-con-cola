b5521f6821abacd13d1cbe1858cd11391d99fdbd
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-25 18:03:59 -0300

    ColaconFrases

--- Colaconfrases/Program.cs
+++ Colaconfrases/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections;
 
 namespace Colaconfrases
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ///Crea un programa que pida frases al usuario, hasta que introduzca una frase vacia.
+            ///En ese momento mostrara todas las ,frases ingresadas
+
+            string frase;
+            int cont = 0;
+
+            Queue miCola = new Queue();  // genera la cola de nombre miCola y la inicia como cola
+
+            do
+            {
+                Console.WriteLine("Ingrese la frase: ");
+                frase = Convert.ToString(Console.ReadLine());
+                miCola.Enqueue(frase);
+                cont = cont + 1;
+            }
+            while (frase != " ");
+
+              Console.WriteLine("Las frases infresadas son: ");
+
+            for (byte i = 0; i < cont + 1; i++) // arma el buclne para recorrer la cola
+            {
+                frase = (string)miCola.Dequeue(); // desencola la cola con Dequeue
+                Console.WriteLine(frase);         // imprime por pantalla
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
+
+
     }
 }
